3f4d251bc7ffe68cb9412a779dfd268cad6aba49
Sirma-cloud <kevinkiplimo99@gmail.com>
2023-07-16 16:56:37 +0300

    Added a delete all Function and mapping

--- src/main/java/com/sirmatests/Main.java
+++ src/main/java/com/sirmatests/Main.java
@@ -2,74 +2,69 @@ package com.sirmatests;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = "com.sirmatests")
-//@EnableAutoConfiguration
-//@Configuration
+@RequestMapping(path = "api/v1/customer")
 @RestController
 public class Main {
-    public static void  main(String[] args) {
+
+    private final  CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-    @GetMapping("/greet")
-    public GreetResponse greet() {
-        GreetResponse response = new GreetResponse(
-                "Hello",
-                List.of("Java", "Golang", "Javascript"),
-                new Person(
-                        "Alex",
-                        28,
-                        30_000)
-        );
-        return response;
-    }
 
-    record  Person(String name, int age, double savings){
+    @GetMapping
+    public List<Customer> getCustomer(){
+        return customerRepository.findAll();
+    }
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ){
 
     }
-    record GreetResponse(
-            String greet,
-            List<String> favProgrammingLanguages,
-            Person person
-    ) {}
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
 
+    record EditCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ){
 
-//    class GreetResponse {
-//        private final String greet;
-//
-//        GreetResponse(String greet) {
-//            this.greet = greet;
-//        }
-//
-//        public String getGreet() {
-//            return greet;
-//        }
-//
-//        @Override
-//        public String toString() {
-//            return "GreetResponse{" +
-//                    "greet='" + greet + '\'' +
-//                    '}';
-//        }
-//
-//        @Override
-//        public boolean equals(Object o) {
-//            if (this == o) return true;
-//            if (o == null || getClass() != o.getClass()) return false;
-//            GreetResponse that = (GreetResponse) o;
-//            return Objects.equals(greet, that.greet);
-//        }
-//
-//        @Override
-//        public int hashCode() {
-//            return Objects.hash(greet);
-//        }
-//    }
+    }
+    @PutMapping("{customerId}")
+    public void editCustomer(@PathVariable("customerId") Integer id, @RequestBody EditCustomerRequest request) {
+        Optional<Customer> optionalCustomer = customerRepository.findById(id);
+        if (optionalCustomer.isPresent()) {
+            Customer customer = optionalCustomer.get();
+            customer.setName(request.name());
+            customer.setEmail(request.email());
+            customer.setAge(request.age());
+            customerRepository.save(customer);
+        }
+    }
+
+    @DeleteMapping("{customerId}")
+    public  void deleteCustomer(@PathVariable("customerId") Integer id) {
+        customerRepository.deleteById(id);
+    }
+
 }
 
